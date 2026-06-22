d06cf6dc835fbd17a4f5b5b7336cfbe50b68d1b4
Luiz <luedukar@gmail.com>
2026-06-22 17:18:49 -0300

    Comentarios no RateLimitMiddleware

--- src/middleware/rateLimitMiddleware.js
+++ src/middleware/rateLimitMiddleware.js
@@ -1,23 +1,28 @@
 const rateLimit = require("express-rate-limit");
 
 function createRateLimiter(
+  // Parâmetros a serem definidos
   windowMs,
   max,
+  // Parâmetro com valor padrão definido
   message = "Aguarde para tentar novamente",
 ) {
   return rateLimit({
+    //Define o número de requisições maxima na rota dentro do intervalo de tempo
     windowMs,
     max,
 
     standardHeaders: true,
     legacyHeaders: false,
 
+    // Mensagem de erro (passou do limite de reqs)
     message: {
       erro: message,
     },
   });
 }
 
+// Ratelimit para login, register, validar duplo-fator, recuperação de conta e reenvio de duplo fator
 const loginLimiter = createRateLimiter(5 * 60 * 1000, 6);
 
 const registerLimiter = createRateLimiter(10 * 60 * 1000, 3);
